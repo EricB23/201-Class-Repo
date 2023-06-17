c96eb5691ff9f90daf72330331b939920c53e281
Eric Baxter <lamarbaxter14@gmail.com>
2023-06-17 14:57:48 -0400

    Added Lighthouse Report to readme

--- JS/script.js
+++ JS/script.js
@@ -7,31 +7,31 @@ alert('Hi' + userName + 'welcome to my page!');
 
 let userPoints = 0;
 birthplace('Was I born in Nashville?');
-function birthPlace(userinput){
+function birthPlace(userinput) {
 
-let answer1 = prompt(userinput);
-console.log('Was I born in Nashville?');
+  let answer1 = prompt(userinput);
+  console.log('Was I born in Nashville?');
 
-}if (answer1 === 'yes' || answer1 === 'y') {
+} if (answer1 === 'yes' || answer1 === 'y') {
   alert('Correct, on to the next question');
-userPoints++;
+  userPoints++;
 
 } else if (answer1 === 'no' || answer1 === 'n') {
   alert('Nope try again');
 }
 else {
   alert('That answer is not valid');
- 
+
 }
 
 birthdate('My birthday is in June?');
 
-function birthdate(userinput){
-let answer2 = prompt(userinput);
+function birthdate(userinput) {
+  let answer2 = prompt(userinput);
 
-}if (answer2 === 'yes' || answer2 === 'y') {
+} if (answer2 === 'yes' || answer2 === 'y') {
 
-  alert('Right again, Keep going!!' );
+  alert('Right again, Keep going!!');
   userPoints++;
 
 } else if (answer2 === 'no' || answer2 === 'n') {
@@ -45,13 +45,13 @@ let answer2 = prompt(userinput);
 
 
 childhood('I was an only child growing up?');
-function childhood(userinput){
-let answer3 = prompt(userinput);
+function childhood(userinput) {
+  let answer3 = prompt(userinput);
 
-}if (answer3 === 'yes' || answer3 === 'y') {
-  
-  alert('It was lonely but your right' );
-  
+} if (answer3 === 'yes' || answer3 === 'y') {
+
+  alert('It was lonely but your right');
+
 } else if (answer3 === 'no' || answer3 === 'n') {
 
   alert('I wish but try again friend.');
@@ -62,12 +62,12 @@ let answer3 = prompt(userinput);
 
 
 sports('I played football and baseball growing up?');
-function sports(userinput){
-let answer4 = prompt(userinput);
+function sports(userinput) {
+  let answer4 = prompt(userinput);
 
-}if (answer4 === 'yes' || answer4 === 'y') {
+} if (answer4 === 'yes' || answer4 === 'y') {
 
-  alert('Ah yes, I loved running back and shortstop' );
+  alert('Ah yes, I loved running back and shortstop');
   userPoints++;
 
 } else if (answer4 === 'no' || answer4 === 'n') {
@@ -75,19 +75,19 @@ let answer4 = prompt(userinput);
 
 
   alert('Man I wouldnt know what id do if you were right');
-}else {
+} else {
   alert('That answer is not valid');
 }
 
 
 
 collegeschool('Did I graduate from Tennessee State University');
-function collegeschool(userinput){
-let answer5 = prompt(userinput);
+function collegeschool(userinput) {
+  let answer5 = prompt(userinput);
 
-}if (answer5 === 'yes' || answer5 === 'y') {
+} if (answer5 === 'yes' || answer5 === 'y') {
 
-  alert('Yes I did, graduate from the The Land Of Golden Sunshine, The best HBCU in the land.' );
+  alert('Yes I did, graduate from the The Land Of Golden Sunshine, The best HBCU in the land.');
   userPoints++;
 
 } else if (answer5 === 'no' || answer5 === 'n') {
@@ -99,35 +99,37 @@ let answer5 = prompt(userinput);
 
 
 
-let number ='';
+let number = '';
 let i = 0;
-while (number !== '6'){
-  console.log('First game :' , i);
-  number = prompt ('guess a number between 1 and 10');
-  if(number < 6){
+while (number !== '6') {
+  console.log('First game :', i);
+  number = prompt('guess a number between 1 and 10');
+  if (number < 6) {
     alert('too low');
-  }else if (number > 6){
+  } else if (number > 6) {
     alert('too high');
-  }else {
+  } else {
     alert('Correct');
-  }userPoints++;
+  } userPoints++;
+  
 }
 i = i + 1;
-if(i === 6){
+if (i === 6) {
   number = '6';
   console.log(number);
 }
 
-for (let i = 0; i < 4; i++){
-  console.log('Second game :' , i);
+for (let i = 0; i < 4; i++) {
+  console.log('Second game :', i);
   let number2 = prompt('Pick my favorite superhero');
-}if(number2 === 'Wolverine'){
-    alert('correct!');
-    userPoints++;
-    break;
-  }else {
-    alert('Wrong, try again!');
-  }
+} if (number2 === 'Wolverine') {
+  alert('correct!');
+  userPoints++;
+  break;
+
+} else {
+  alert('Wrong, try again!');
+}
 
 console.log(userPoints);
 
@@ -135,5 +137,4 @@ alert(`You have gained ${userPoints} points`);
 
 
 alert('You have ${userPoints}');
-
 
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # 201-Class-Repo
+
 Lab 04
 Driver:Shekinah Christian
 Navigator: Eric Baxter
-We worked on the code together in turning our questions in the game into functions 
+We worked on the code together in turning our questions in the game into functions
+
+Lab 5 Lighthouse report
+![Alt text](<Screenshots/Screen Shot 2023-06-17 at 2.46.35 PM.png>)
